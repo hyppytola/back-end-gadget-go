e20dd0f49a73f1433a2d1bf5e7f09004c065c209
hyppytola <domolakova.02@gmail.com>
2022-04-29 06:37:49 +0600

    add payment and cart backend

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+//var bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    cart: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Users', userSchema)
